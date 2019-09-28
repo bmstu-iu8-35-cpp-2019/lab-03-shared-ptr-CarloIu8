ced51c96bf70624b2336f12e424cd0dcd16ebc1b
Kirill <kirillpo2016@mail.ru>
2019-09-29 01:48:05 +0300

    first try

--- include/header.hpp
+++ include/header.hpp
@@ -2,5 +2,105 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+#include <atomic>
+#include <exception>
+#include <iostream>
 
-#endif // INCLUDE_HEADER_HPP_
+template <typename T>
+class SharedPtr {
+ public:
+  SharedPtr() {
+    ptr = nullptr;
+    number = nullptr;
+  }
+  SharedPtr(T* pt) {
+    ptr = pt;
+    number = new std::atomic<int>;
+    *number = 1;
+  }
+  SharedPtr(const SharedPtr& r) {
+    if (r) {
+      ptr = r.ptr;
+      number = r.number;
+      (*number)++;
+    }
+  }
+  ~SharedPtr() {
+    if (*number == 1) {
+      reset();
+    } else if (*number > 1) {
+      (*number)--;
+    }
+  }
+  // SharedPtr(SharedPtr&& r);
+  SharedPtr& operator=(const SharedPtr& r) {
+    if (*this && r) {
+      (*number)--;
+      ptr = r.ptr;
+      number = r.number;
+      (*number)++;
+    } else {
+      ptr = r.ptr;
+      number = r.number;
+      (*number)++;
+    }
+    return *this;
+  }
+  operator bool() const {
+    if (ptr == nullptr || number == nullptr) return false;
+    return true;
+  }
+  T& operator*() const { return *ptr; }
+  T* operator->() const { return ptr; }
+
+  T* get() { return ptr; }
+  void reset() {
+    if (*this) {
+      if (*number == 1) {
+        delete number;
+        delete ptr;
+        ptr = nullptr;
+        number = nullptr;
+      } else if (*number > 1) {
+        (*number)--;
+      }
+    }
+  }
+  void vivod() {
+    std::cout << "Value " << *ptr << " "
+              << "Count ";
+    std::cout << use_count() << std::endl;
+  }
+  void reset(T* pt) {
+    if (*this) {
+      *number--;
+      if (*number == 0) {
+        delete number;
+        delete ptr;
+        ptr = nullptr;
+        number = nullptr;
+      }
+      ptr = pt;
+      number = new std::atomic<int>;
+      *number = 1;
+    } else {
+      ptr = pt;
+      number = new std::atomic<int>;
+      *number = 1;
+    }
+  }
+  void swap(SharedPtr& r) {
+    SharedPtr a;
+    a = r;
+    r = *this;
+    *this = a;
+  }
+  // возвращает количество объектов SharedPtr, которые ссылаются на тот же
+  // управляемый объект
+  size_t use_count() const { return static_cast<size_t>(*number); }
+
+ private:
+  T* ptr;
+  std::atomic<int>* number;
+};
+#endif  // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -3,3 +3,49 @@
 #include <gtest/gtest.h>
 
 #include <header.hpp>
+
+TEST(SHARED_PTR, init) {
+  SharedPtr<int> ptr;
+  EXPECT_FALSE(ptr);
+  SharedPtr<int> p(new int(5));
+  EXPECT_EQ(p.use_count(), 1);
+  EXPECT_EQ(*p, 5);
+  EXPECT_TRUE(p);
+  SharedPtr<int> p1(p);
+  EXPECT_EQ(p.use_count(), p1.use_count());
+  EXPECT_EQ(p.use_count(), 2);
+  EXPECT_EQ(*p1, 5);
+  EXPECT_TRUE(p1);
+}
+
+TEST(SHARED_PTR, assign) {
+  SharedPtr<int> p(new int(8));
+  SharedPtr<int> p1(new int(2));
+  p = p1;
+  EXPECT_EQ(p.use_count(), p1.use_count());
+  EXPECT_EQ(p.use_count(), 2);
+  EXPECT_EQ(*p1, *p);
+  EXPECT_TRUE(p1);
+}
+
+TEST(SHARED_PTR, test_of_reset) {
+  SharedPtr<int> p(new int(3));
+  SharedPtr<int> p1(p);
+  EXPECT_EQ(p.use_count(), 2);
+  p.reset();
+  EXPECT_FALSE(p);
+  EXPECT_EQ(p1.use_count(), 1);
+  p1.reset(new int(10));
+  EXPECT_EQ(p1.use_count(), 1);
+  EXPECT_EQ(*p1, 10);
+}
+
+TEST(SHARED_PTR, test_of_swap) {
+  SharedPtr<int> p(new int(3));
+  SharedPtr<int> p1(new int(10));
+  p1.swap(p);
+  EXPECT_EQ(p1.use_count(), 1);
+  EXPECT_EQ(p.use_count(), 1);
+  EXPECT_EQ(*p1, 3);
+  EXPECT_EQ(*p, 10);
+}
